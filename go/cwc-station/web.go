4a5c07186f81eab10ac9a852281c0ca7fea9e223
Andrew Amos <m0vva@protonmail.ch>
2019-06-25 08:30:11 +0100

    updated to use go-socket.io

--- go/cwc-station/web.go
+++ go/cwc-station/web.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/glog"
+	"github.com/googollee/go-socket.io"
 )
 
 const APIPort = ":8080"
@@ -40,6 +41,44 @@ var html = template.Must(template.New("https").Parse(`
 `))
 
 func WebServer(ctx context.Context, config *cwc.Config) {
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		return "recv " + msg
+	})
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+	server.OnError("/", func(e error) {
+		fmt.Println("meet error:", e)
+	})
+	server.OnDisconnect("/", func(s socketio.Conn, msg string) {
+		fmt.Println("closed", msg)
+	})
+	go server.Serve()
+	defer server.Close()
+
+	// http.Handle("/socket.io/", server)
+	// http.Handle("/", http.FileServer(http.Dir("./asset")))
+	// log.Println("Serving at localhost:8000...")
+	// log.Fatal(http.ListenAndServe(":8000", nil))
+
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
@@ -55,5 +94,6 @@ func WebServer(ctx context.Context, config *cwc.Config) {
 			"status": "success",
 		})
 	})
+	router.GET("/socket.io", gin.WrapH(server))
 	router.RunTLS(APIPort, "./certs/server.pem", "./certs/server.key")
 }
